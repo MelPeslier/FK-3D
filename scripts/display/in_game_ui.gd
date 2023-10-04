c13f4a8748bb51d01fecd36e201662593d89febd
MelPeslier <mel.peslier@gmail.com>
2023-10-04 17:01:03 -0300

    positions of 3d sdisplay only done

--- scripts/display/in_game_ui.gd
+++ scripts/display/in_game_ui.gd
@@ -18,9 +18,7 @@ enum Anchor {
 @export var conversion: Conversion
 @export var pixel_per_meter := 200
 @export var body: MeshInstance3D
-@export_range(0.5, 1, 0.05) var coef_distance := 0.5
-@export_range(-0.15, 1.15, 0.05) var coef_height: float
-
+@export var margin := Vector3(0.1, 0.1, 0.1)
 @export var anchor_x: Anchor
 @export var anchor_y: Anchor
 @export var anchor_z: Anchor
@@ -56,11 +54,10 @@ func _on_frame_post_draw() -> void:
 	# Update the scale of the rendering mesh
 	match conversion:
 		Conversion.SIZE:
-			print(sub_viewport.size.y)
 			display_mesh.mesh.size = sub_viewport.size / float(pixel_per_meter)
+			
 #			print("\n\n" + str (parent.name))
 #			print("%f / %f = %f and needs to be : %f" % [sub_viewport.size.y, pixel_per_meter, display_mesh.mesh.size.y, sub_viewport.size.y / float(pixel_per_meter)])
-			Vector2(2, 6.165)
 		
 		Conversion.SCALE:
 			var _scale: float = sub_viewport.size.x * 1.0 / sub_viewport.size.y
@@ -77,42 +74,30 @@ func _on_frame_post_draw() -> void:
 	RenderingServer.frame_post_draw.disconnect(_on_frame_post_draw)
 
 
-func _update_position(width: float, height: float, depth: float, _scale: Vector3) -> Vector3:
+func _update_position(_x: float, _y: float, _z: float, _scale: Vector3) -> Vector3:
+	var target_position := Vector3.ZERO
+	
 	var display_mesh = $DisplayMesh
+	var _body := Vector3(_x, _y, _z) * _scale
+	var ui := Vector2(display_mesh.mesh.size.x * display_mesh.scale.x, display_mesh.mesh.size.y * display_mesh.scale.y)
 	
-	var display_mesh_y: float = display_mesh.mesh.size.y * display_mesh.scale.y
-	var height_y := height * _scale.y
+	target_position.x = _calculate_position(ui.x, _body.x, anchor_x, margin.x)
+	target_position.z = _calculate_position(0, _body.z, anchor_z, margin.z)
 	
-	# Choose yourself the position of the label with the coef
-	# For < 0 and for > 1, the coef is a margin
-	var target_y: float
-	if coef_height < 0:
-		target_y = - (display_mesh_y / 2.0 ) + coef_height
-	elif coef_height > 1:
-		target_y = (height_y + display_mesh_y / 2.0) + coef_height - 1
+	# If the UI is bigger than the mesh itself
+	if ui.y > _body.y:
+		print("default position")
+		target_position.y = (ui.y - _body.y) / 2.0 + margin.y
 	else:
-		target_y = height_y * coef_height
-	
-	if display_mesh_y > height_y:
-		print("oui")
-		target_y = display_mesh_y - height_y
-		
-	
-	print("body mesh : %.f  |  display mesh : %.f" % [height, display_mesh.mesh.size.y])
-	print("height: %f  | mesh_size: %f" % [height_y,  display_mesh_y])
-	
-	var big_xz := depth
-	var big_scale := _scale.z
-	if width > depth:
-		big_xz = width
-		big_scale = _scale.x
-	
-	var target_x: float = (big_xz * big_scale + display_mesh.mesh.size.x * display_mesh.scale.x) * coef_distance
-	
-	var target_position := Vector3(-1 * target_x, target_y, 0)
+		target_position.y = _calculate_position(ui.y, _body.y, anchor_y, margin.y)
 	
 	return target_position
 
+
+func _calculate_position(_ui: float, _body_2: float, _anchor: Anchor, _margin: float) -> float:
+	var result: float
+	result = _anchor * (_body_2 + _ui + _margin) / 2.0
+	return result
 
 
 
