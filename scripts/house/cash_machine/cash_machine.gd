52df1f2f5566e1005260d99f7ebdc0afd4144ed6
MelPeslier <mel.peslier@gmail.com>
2023-12-01 12:17:25 -0300

    removed agent meshsh for ath finding ?? +  merged game_data into cash_machine

--- scripts/house/cash_machine/cash_machine.gd
+++ scripts/house/cash_machine/cash_machine.gd
@@ -1,14 +1,38 @@
+class_name CashMachine
 extends Node3D
 
-@export var game_data: GameData
+signal update_money(money: float)
+
+var money: float:
+	set(new_value):
+		money = new_value
+		update_money.emit(money)
 
 @onready var label_3d: Label3D = $Label3D
 
 
 func _ready() -> void:
-	game_data.update_money.connect(_on_update_money)
+	update_money.connect(_on_update_money)
+	money = 0
+
+
+func can_buy(price: float) -> bool:
+	return price < money
+
+
+func buy(price: float) -> void:
+	money -= price
+
+
+func sell(price: float) -> void:
+	money += price
+
+
+func steal(amount: float) -> float:
+	var money_gone := minf(amount, money)
+	money -= money_gone
+	return money_gone
 
 
 func _on_update_money(money: float) -> void:
-	print("money received" + str(money) )
 	label_3d.text = "%.2f %s" % [money, "€"]
